1e2d9486741668f7e2ea0141057eb2eae07be173
FernandoDuffoo <fduffoo@aol.com>
2024-05-08 17:08:06 -0400

    update

--- app.py
+++ app.py
@@ -10,6 +10,22 @@ API_KEY = '5a96da934f-3d98927201-sd4x8o'
 BASE_URL = 'https://open.er-api.com/v6/latest/'
 
 
+def validate_currency_code(currency_code):
+    # Perform basic validation for currency codes
+    if not currency_code.isalpha() or len(currency_code) != 3:
+        return False
+    return True
+
+
+def validate_amount(amount):
+    # Perform validation for the amount
+    try:
+        float(amount)
+        return True
+    except ValueError:
+        return False
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -17,9 +33,13 @@ def index():
 
 @app.route('/convert', methods=['POST'])
 def convert():
-    from_currency = request.form['from_currency']
-    to_currency = request.form['to_currency']
-    amount = float(request.form['amount'])
+    from_currency = request.form['from_currency'].upper()
+    to_currency = request.form['to_currency'].upper()
+    amount = request.form['amount']
+
+    # Validate input data
+    if not (validate_currency_code(from_currency) and validate_currency_code(to_currency) and validate_amount(amount)):
+        return 'Invalid input data.'
 
     # Make a request to the Open Exchange Rates API to get latest exchange rates
     response = requests.get(f'{BASE_URL}{from_currency}?apikey={API_KEY}')
@@ -30,7 +50,7 @@ def convert():
         if 'rates' in data:
             conversion_rate = data['rates'].get(to_currency)
             if conversion_rate is not None:
-                converted_amount = amount * conversion_rate
+                converted_amount = float(amount) * conversion_rate
                 return render_template('result.html', from_currency=from_currency, to_currency=to_currency,
                                        amount=amount, converted_amount=converted_amount)
             else:
