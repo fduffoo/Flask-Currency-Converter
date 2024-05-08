db1f69eb161eb5cb408e2583471082a38e9cdca5
FernandoDuffoo <fduffoo@aol.com>
2024-05-08 16:55:43 -0400

    added some style

--- app.py
+++ app.py
@@ -23,14 +23,22 @@ def convert():
 
     # Make a request to the Open Exchange Rates API to get latest exchange rates
     response = requests.get(f'{BASE_URL}{from_currency}?apikey={API_KEY}')
-    data = response.json()
 
-    if to_currency in data['rates']:
-        conversion_rate = data['rates'][to_currency]
-        converted_amount = amount * conversion_rate
-        return render_template('result.html', from_currency=from_currency, to_currency=to_currency, amount=amount, converted_amount=converted_amount)
+    if response.status_code == 200:
+        data = response.json()
+
+        if 'rates' in data:
+            conversion_rate = data['rates'].get(to_currency)
+            if conversion_rate is not None:
+                converted_amount = amount * conversion_rate
+                return render_template('result.html', from_currency=from_currency, to_currency=to_currency,
+                                       amount=amount, converted_amount=converted_amount)
+            else:
+                return 'Invalid currency code.'
+        else:
+            return 'Invalid currency code.'
     else:
-        return 'Invalid currency code.'
+        return 'Error accessing currency conversion API.'
 
 
 if __name__ == '__main__':
